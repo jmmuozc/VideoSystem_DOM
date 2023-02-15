db9e5bfec2c2d1a3cdba81088ad72b3594f26bd1
jmmuozc <josemiguelmunoz013@gmail.com>
2023-02-15 22:47:02 +0100

    fixed all ERRORS

--- MVC/VideoSystemView.js
+++ MVC/VideoSystemView.js
@@ -6,7 +6,6 @@ class videoSystemView {
         this.main = document.getElementsByTagName("main")[0];
     }
 
-
     showCategories(categoriesList) {
 
         if (document.getElementById("div-categories")) this.main.removeChild(document.getElementById("div-categories"));
@@ -27,22 +26,49 @@ class videoSystemView {
             let categoriesColumn = document.createElement("div");
             categoriesColumn.classList.add("col");
             categoriesColumn.innerHTML = `<img src='./media/${category.Name}.jpg' width=200 height=100 id='categoryImg'>
-            <h5>${category.Name}</h5>`;
+            <h3>${category.Name}</h3>`;
             categoriesRow.appendChild(categoriesColumn);
         }
     }
 
+    headerCategories(categoriesList) {
+        let nav = document.getElementById("navbarUl");
+        if (document.getElementById("nav-categories")) nav.removeChild(document.getElementById("nav-categories"));
+        let categoriesIl = document.createElement("li");
+        categoriesIl.classList.add("nav-item");
+        categoriesIl.classList.add("dropdown");
+        categoriesIl.setAttribute("Id", "nav-categories")
+        categoriesIl.innerHTML = `<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">Categorias</a>`;
+
+        nav.appendChild(categoriesIl);
+        let categoriesUl = document.createElement("ul");
+        categoriesUl.setAttribute("Id", "categories-ul")
+        categoriesUl.classList.add("dropdown-menu");
+        categoriesIl.appendChild(categoriesUl);
+
+        for (let category of categoriesList) {
+            let categoryLink = document.createElement("li");
+            categoryLink.innerHTML = `<a class="dropdown-item" href="#">${category.Name}</a>`;
+            categoriesUl.appendChild(categoryLink);
+        }
+    }
+
     rngProductions(productionsList) {
+
+        if (document.getElementById("div-rngProductions")) this.main.removeChild(document.getElementById("div-rngProductions"));
+
         // let arrayProductions=
         let arrayProductions = [];
         let rng;
-        const numberProductions=3;
+        const numberProductions = 3;
         let arrayExistent = [];
         // Creamos un elemento Div
         let productionsContainer = document.createElement("div");
         // Le añadimos una clase (container)
         productionsContainer.classList.add("container");
         productionsContainer.classList.add("text-center"); 
+        productionsContainer.setAttribute("Id", "div-rngProductions");
         this.main.appendChild(productionsContainer);
 
         for (let production of productionsList) {
@@ -62,9 +88,25 @@ class videoSystemView {
             arrayExistent.push(rng);
             let productionsColumn = document.createElement("div");
             productionsColumn.classList.add("col");
-            productionsColumn.innerHTML = `<img src='./media/${arrayProductions[rng].Image}' width=200 height=100 id='productionImg'>
-            <h5>${arrayProductions[rng].Title}</h5>`;
+            productionsColumn.innerHTML = `<div class="card mx-auto" style="width: 18rem;">
+            <img src='./media/${arrayProductions[rng].Image}' class="card-img-top" alt="${arrayProductions[rng].Image}" width=250 height=150>
+            <div class="card-body">
+              <h5 class="card-title">${arrayProductions[rng].Title}</h5>
+              <a href="#" class="btn btn-primary">Ver</a>
+            </div>
+          </div>`
+
             productionsRow.appendChild(productionsColumn);
+        }
+    }
+
+    
+	bindInit(handler) {
+        for (let element of document.getElementsByClassName('init')) {
+            element.addEventListener("click", (event) => {
+                handler();
+            });
+           
         }
     }
 
